c0c75985f620946bc838ef3c567c8afa881e5cf4
shuntagami <shuntagami23@gmail.com>
2022-12-22 22:35:31 +0900

    add IAM User group

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ Account alias
 resource "aws_iam_account_alias" "alias" {
   account_alias = "shuntagami"
 }
+
+/*====
+IAM User group
+======*/
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
