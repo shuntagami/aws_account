decd98ef22cadb39f20c668cc7ad28b9b4352bf9
shuntagami <shuntagami23@gmail.com>
2022-12-22 22:44:56 +0900

    add aws_iam_group_policy_attachment
    executed below
    $ terraform import aws_iam_group_policy_attachment.user_group_policy developers/arn:aws:iam::aws:policy/ReadOnlyAccess
    $ make apply

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ IAM User group
 resource "aws_iam_group" "developers" {
   name = "developers"
 }
+
+resource "aws_iam_group" "admin" {
+  name = "admin"
+}
+
+resource "aws_iam_group_policy_attachment" "user_group_policy" {
+  group      = aws_iam_group.developers.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "admin_group_policy" {
+  group      = aws_iam_group.admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
