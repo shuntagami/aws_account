875cf7c7219503ec99d15c0ff3d10124b51159a0
shuntagami <shuntagami23@gmail.com>
2022-12-24 13:02:41 +0900

    enforce mfa

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ resource "aws_iam_account_alias" "alias" {
 /*====
 IAM User group
 ======*/
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_group" "developers" {
   name = "developers"
 }
@@ -24,6 +26,17 @@ resource "aws_iam_group_policy_attachment" "user_group_policy" {
 resource "aws_iam_group_policy_attachment" "admin_group_policy" {
   group      = aws_iam_group.admin.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+module "enforce_mfa" {
+  source = "terraform-module/enforce-mfa/aws"
+
+  policy_name                     = "managed-mfa-enforce"
+  account_id                      = data.aws_caller_identity.current.id
+  groups                          = [aws_iam_group.developers.name, aws_iam_group.admin.name]
+  manage_own_signing_certificates = true
+  manage_own_ssh_public_keys      = true
+  manage_own_git_credentials      = true
 }
 
 /*====
